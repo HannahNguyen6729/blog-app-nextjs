0aa485c8460b44d052b8f10be8f7d87e872ca97f
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-09-20 14:06:41 +0300

    update layout, add components folder

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
 import Link from 'next/link';
 import styles from './page.module.css';
+import CategoryList from '@/components/categoryList/CategoryList';
+import Featured from '@/components/featured/Featured';
+import CardList from '@/components/cardList/CardList';
+import Menu from '@/components/menu/Menu';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div>
-        <Link href="/">homepage</Link>
+    <main className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
       </div>
     </main>
   );
