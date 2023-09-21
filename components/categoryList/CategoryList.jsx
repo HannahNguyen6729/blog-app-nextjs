a8ccec8b6e4552401006fac5061f0328071780d8
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-09-21 16:59:25 +0300

    add category component design

--- components/categoryList/CategoryList.jsx
+++ components/categoryList/CategoryList.jsx
@@ -1,8 +1,51 @@
 import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
 import styles from './categoryList.module.css';
 
-const CategoryList = () => {
-  return <div className={styles.container}>CategoryList</div>;
+const CategoryList = async () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Popular Categories</h1>
+      <div className={styles.categories}>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.style}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+
+        <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/fashion.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+        <Link href="/blog" className={`${styles.category} ${styles.style}`}>
+          <Image
+            src="/food.png"
+            alt=""
+            width={32}
+            height={32}
+            className={styles.image}
+          />
+          style
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default CategoryList;
