fdfd6aa322d7f230bc16ff801e987f1fdb5c9fae
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-09-21 16:10:45 +0300

    fix error: prop style did not match

--- context/Theme.jsx
+++ context/Theme.jsx
@@ -1,12 +1,19 @@
 'use client';
 
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { ThemeContext } from './ThemeContextProvider';
 
 const Theme = ({ children }) => {
   const [isDarkTheme] = useContext(ThemeContext);
-  console.log({ isdark: isDarkTheme });
-  return <div className={isDarkTheme ? 'dark' : 'light'}>{children}</div>;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (mounted) {
+    return <div className={isDarkTheme ? 'dark' : 'light'}>{children}</div>;
+  }
 };
 
 export default Theme;
