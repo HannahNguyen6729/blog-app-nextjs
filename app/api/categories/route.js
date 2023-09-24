2028b3631c64f40e9c4d73209f0daf22adc64661
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-09-24 20:03:18 +0300

    create GET request handler for categories route

---
+++ app/api/categories/route.js
@@ -0,0 +1,18 @@
+import prisma from '@/utils/connect';
+import { NextResponse } from 'next/server';
+
+export const GET = async () => {
+  try {
+    const categories = await prisma.category.findMany();
+    return NextResponse.json({
+      status: 200,
+      categories,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: 'Internal Server Error' },
+      { status: 500 }
+    );
+  }
+};
