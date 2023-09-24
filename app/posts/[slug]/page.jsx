6753832f849a57057fda703848f73486123b92a7
Hanh Nguyen <myhanh.nguyen.969317598@gmail.com>
2023-09-24 22:54:47 +0300

    create getSinglePost handler for singlePost route

--- app/posts/[slug]/page.jsx
+++ app/posts/[slug]/page.jsx
@@ -4,38 +4,56 @@ import Menu from '@/components/menu/Menu';
 
 import styles from './singlePage.module.css';
 
-const SinglePage = async () => {
+const getData = async (slug) => {
+  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+};
+
+const SinglePage = async ({ params }) => {
+  const data = await getData(params.slug);
+
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>new post</h1>
+          <h1 className={styles.title}> {data?.title} </h1>
           <div className={styles.user}>
-            <div className={styles.userImageContainer}>
-              <Image src="/food.png" alt="" fill className={styles.avatar} />
-            </div>
+            {data?.user?.image && (
+              <div className={styles.userImageContainer}>
+                <Image
+                  src={data.user.image}
+                  alt="avatar"
+                  fill
+                  className={styles.avatar}
+                />
+              </div>
+            )}
 
             <div className={styles.userTextContainer}>
-              <span className={styles.username}>Tom Smith</span>
-              <span className={styles.date}>01.01.2024</span>
+              <span className={styles.username}> {data?.user?.name} </span>
+              <span className={styles.date}>24.09.2023</span>
             </div>
           </div>
         </div>
 
-        <div className={styles.imageContainer}>
-          <Image src="/food.png" alt="" fill className={styles.image} />
-        </div>
+        {data?.img && (
+          <div className={styles.imageContainer}>
+            <Image src={data.img} alt="" fill className={styles.image} />
+          </div>
+        )}
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
           <div className={styles.description} />
           <div className={styles.comment}>
-            Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet,
-            consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor
-            sit amet, consecteturLorem ipsum dolor sit amet, consecteturLorem
-            ipsum dolor sit amet, consecteturLorem ipsum dolor sit amet,
-            consecteturLorem ipsum dolor sit amet, consecteturLorem ipsum dolor
-            sit amet, consecteturLorem ipsum dolor sit amet, consectetur
+            {data?.desc}
             <Comments />
           </div>
         </div>
